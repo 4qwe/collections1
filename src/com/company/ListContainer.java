05e757d21a9ff1966c3b8d6f3a9d1f22fedbe7a3
zartre <paulpreuss@gmail.com>
2020-03-24 14:55:23 +0100

    First commit new branch, broken HashMap

--- src/com/company/ListContainer.java
+++ src/com/company/ListContainer.java
@@ -1,21 +1,25 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
+//Klasse, die alle Elemente und Methoden meiner ArrayList beinhaltet
 public class ListContainer {
 
     public int anzahl_elemente = -1;
-    public ArrayList<String> listerio = null;
+    public HashMap hashmapio = null;
 
+    //
     public int promptWieviele() {
         Scanner scanObj = new Scanner(System.in);
         System.out.println("Wieviele Elemente werden es denn?");
         return scanObj.nextInt();
     }
 
-    public ArrayList<String> createAL(int i) {
-        ArrayList<String> lisi = new ArrayList<>();
+    public HashMap(int i) {
+        HashMap<> lisi = new ArrayList<>();
+        //der Loop befüllt das Array mit "a String" an jeder Stelle der Liste
+        //"add" aus Java 'List' wird verwendet
         while (i > 0) {
             lisi.add("a String");
             i--;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Beispielprojekt dass ArrayList in irgendeiner sinnvollen Weise verwendet
+        // Beispielprojekt, dass ArrayList in irgendeiner sinnvollen Weise verwendet
 
-        ListContainer Containero = new ListContainer(); //das Objekt unserer Klasse heißt hardcoded "Containero"
+        ListContainer containero = new ListContainer(); /*das Objekt unserer Klasse heißt hardcoded "containero" -
+        Variablen-Namen in main können als hardcoded betrachtet werden (?), weil main wird niemals instantiiert werden*/
 
-        Containero.anzahl_elemente = Containero.promptWieviele(); // erst die Variable im Objekt erstellen, sie wird dann mehrfach benutzt
+        containero.anzahl_elemente = containero.promptWieviele(); // erst die Variable im Objekt erstellen, sie wird dann mehrfach benutzt
 
-        System.out.printf("Wir bauen also eine ArrayList mit %d Elementen%n", Containero.anzahl_elemente);
+        System.out.printf("Wir bauen also eine ArrayList mit %d Elementen%n", containero.anzahl_elemente);
+        containero.listerio = containero.createAL(containero.anzahl_elemente);
 
-        Containero.listerio = Containero.createAL(Containero.anzahl_elemente);
-        System.out.printf("Unser Array hat somit %s Stellen\n", Containero.listerio.size());
-        System.out.printf("Und zum Beispiel an 1. Stelle: '%s'\n", Containero.listerio.get(0));
+        System.out.printf("Unser Array hat somit %s Stellen\n", containero.listerio.size());
+        System.out.printf("Und zum Beispiel an 16. Stelle: '%s'\n", containero.listerio.get(15));
 
         System.out.println("ENDE");
 
