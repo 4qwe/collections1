b048ef2990445f39d05a4dcbdb60c3acb7461fed
zartre <paulpreuss@gmail.com>
2020-03-27 17:58:59 +0100

    Typos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,17 @@ public class Main {
     public static void main(String[] args) {
         // Beispielprojekt dass ArrayList in irgendeiner sinnvollen Weise verwendet
 
-        ListContainer Containero = new ListContainer(); //das Objekt unserer Klasse heißt hardcoded "Containero"
+        ListContainer containero = new ListContainer(); //das Objekt unserer Klasse heißt hardcoded "containero"
 
-        Containero.anzahl_elemente = Containero.promptWieviele(); // erst die Variable im Objekt erstellen, sie wird dann mehrfach benutzt
+        containero.anzahl_elemente = containero.promptWieviele(); // erst die Variable im Objekt erstellen, sie wird dann mehrfach benutzt
 
-        System.out.printf("Wir bauen also eine ArrayList mit %d Elementen%n", Containero.anzahl_elemente);
+        System.out.printf("Wir bauen also eine ArrayList mit %d Elementen%n", containero.anzahl_elemente);
 
-        Containero.listerio = Containero.createAL(Containero.anzahl_elemente);
-        System.out.printf("Unser Array hat somit %s Stellen\n", Containero.listerio.size());
-        System.out.printf("Und zum Beispiel an 1. Stelle: '%s'\n", Containero.listerio.get(0));
+        containero.listerio = containero.createAL(containero.anzahl_elemente);
+        System.out.printf("Unser Array hat somit %s Stellen\n", containero.listerio.size());
+        System.out.printf("Und zum Beispiel an 1. Stelle: '%s'\n", containero.listerio.get(0));
 
         System.out.println("ENDE");
-
     }
 
 }
